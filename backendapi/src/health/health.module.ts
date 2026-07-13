38771bb21eac382f470ecf12bdab1e74fc2eb8d6
mdkhalid <khalid.bib@gmail.com>
2026-07-13 17:03:41 +0530

    fix: import MonitoringModule in HealthModule for MetricsService

--- backendapi/src/health/health.module.ts
+++ backendapi/src/health/health.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { HealthController } from './health.controller';
 import { CacheService } from '../common/cache.service';
 import { JobQueueService } from '../jobs/job-queue.service';
+import { MonitoringModule } from '../monitoring/monitoring.module';
 
 @Module({
+  imports: [MonitoringModule],
   controllers: [HealthController],
   providers: [CacheService, JobQueueService],
   exports: [CacheService, JobQueueService],
